f1e38744871f2908f3879f53460dbb6979eb9389
kunalkjaiswal <contact.kunal13@gmail.com>
2024-03-06 14:15:53 +0530

    Removed additional spaces

--- blocks/form/functions.js
+++ blocks/form/functions.js
@@ -7,10 +7,10 @@
  */
 function getFullName(firstname, lastname) {
   // eslint-disable-next-line no-param-reassign
-  firstname = (firstname == null) ? '' : firstname;
+  firstname = firstname == null ? "" : firstname;
   // eslint-disable-next-line no-param-reassign
-  lastname = (lastname == null) ? '' : lastname;
-  return firstname.concat(' ').concat(lastname);
+  lastname = lastname == null ? "" : lastname;
+  return firstname.concat(" ").concat(lastname);
 }
 
 /**
@@ -24,34 +24,31 @@ const createLabelInElement = (elementSelector, labelClass) => {
    * The main element in the DOM where the form resides.
    * @type {HTMLElement}
    */
-  const mainEl = document.getElementsByTagName('main')[0]; 
-  
+  const mainEl = document.getElementsByTagName("main")[0];
   /**
    * The form element containing the target element.
    * @type {HTMLElement}
    */
-  const formEl = mainEl.querySelector('form');
-
+  const formEl = mainEl.querySelector("form");
   /**
    * The target element to which the label will be appended.
    * @type {HTMLElement}
    */
   const element = formEl.querySelector(elementSelector);
-  
   if (!element) {
     console.error(`Element with selector '${elementSelector}' not found.`);
     return;
   }
-
   /**
    * The text content of the label element.
    * @type {string}
    */
-  const labelText = element.getElementsByTagName('label')[0].innerHTML;
-  element.getElementsByTagName('label')[0].innerHTML = '';
-
+  const labelText = element.getElementsByTagName("label")[0].innerHTML;
+  element.getElementsByTagName("label")[0].innerHTML = "";
   if (!labelText) {
-    console.error(`No data-label attribute found for element with selector '${elementSelector}'.`);
+    console.error(
+      `No data-label attribute found for element with selector '${elementSelector}'.`
+    );
     return;
   }
 
@@ -59,19 +56,21 @@ const createLabelInElement = (elementSelector, labelClass) => {
    * The newly created label element.
    * @type {HTMLLabelElement}
    */
-  const labelElement = document.createElement('label');
+  const labelElement = document.createElement("label");
   labelElement.classList.add(labelClass);
   labelElement.textContent = labelText;
   element.appendChild(labelElement);
 };
-
 
 /**
  * On Wizard Init.
  * @name onWizardInit Runs on initialization of wizard
  */
 function onWizardInit() {
-  createLabelInElement('.form-permanentaddresstoggle', 'permanent-address-toggle__label');
+  createLabelInElement(
+    ".form-permanentaddresstoggle",
+    "permanent-address-toggle__label"
+  );
 }
 
 // eslint-disable-next-line import/prefer-default-export
