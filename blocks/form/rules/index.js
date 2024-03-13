a2c4278ca9c86dd30c13193da1497d4add0e16c3
Nitin Gupta <nitigupt@adobe.com>
2024-03-13 16:53:55 +0530

    remove fetchdata temporarily as its giving 404 on local

--- blocks/form/rules/index.js
+++ blocks/form/rules/index.js
@@ -223,7 +223,7 @@ async function fetchData({ id }) {
 }
 
 export async function initAdaptiveForm(formDef, createForm) {
-  const data = await fetchData(formDef);
+  const data = null;
   await registerCustomFunctions();
   const form = await initializeRuleEngineWorker({
     ...formDef,
