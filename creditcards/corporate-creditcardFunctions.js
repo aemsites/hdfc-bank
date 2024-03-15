8fa3c39f139245eaa58ea0f862e8363efa7cfc4a
bazilMuralidharanDev <bazil.m@dizitalpods.com>
2024-03-15 14:56:46 +0530

    baseCode in corporate creditFunction and Lint issue is not cleared

--- creditcards/corporate-creditcardFunctions.js
+++ creditcards/corporate-creditcardFunctions.js
@@ -1,3 +1,6 @@
+/* eslint-disable prefer-destructuring */
+/* eslint-disable no-unused-vars */
+/* eslint-disable max-len */
 /* eslint no-console: ["error", { allow: ["warn", "error", "log"] }] */
 import createJourneyId from '../common/journey-utils.js';
 import {
@@ -22,6 +25,10 @@ const otpGenSuccess = (res, globals) => {
     login: globals.form.loginPanel,
     otp: globals.form.otpPanel,
     otpButton: globals.form.getOTPbutton,
+    currentAddressNTB: globals.form.corporateCardWizardView.yourDetailsPanel.yourDetailsPage.currentDetails.currentAddressNTB,
+    currentAddressETB: globals.form.corporateCardWizardView.yourDetailsPanel.yourDetailsPage.currentDetails.currentAddressETB,
+    panWizardField: globals.form.corporateCardWizardView.yourDetailsPanel.yourDetailsPage.personalDetails.dobPersonalDetails,
+    dobWizardField: globals.form.corporateCardWizardView.yourDetailsPanel.yourDetailsPage.personalDetails.panNumberPersonalDetails,
   };
   currentFormContext.isCustomerIdentified = res?.customerIdentificationResponse?.CustomerIdentificationResponse?.errorCode === '0' ? 'Y' : 'N';
   const welcomeTxt = formUtil(globals, pannel.welcome);
@@ -30,10 +37,30 @@ const otpGenSuccess = (res, globals) => {
   const loginPanel = formUtil(globals, pannel.login);
   const regMobNo = pannel.login.registeredMobileNumber.$value;
 
+  const panWizardField = formUtil(globals, pannel.panWizardField);
+  const dobWizardField = formUtil(globals, pannel.dobWizardField);
+  const currentAddressNTB = formUtil(globals, pannel.currentAddressNTB);
+  const currentAddressETB = formUtil(globals.pannel.currentAddressETB);
+
   welcomeTxt.visible(false);
   otpBtn.visible(false);
   loginPanel.visible(false);
   otpPanel.visible(true);
+
+  if (pannel.login.pan.$value) {
+    panWizardField.visible(true);
+    dobWizardField.visible(false);
+  } else {
+    panWizardField.visible(false);
+    dobWizardField.visible(true);
+  }
+  if (currentFormContext.existingCustomer === 'Y') {
+    currentAddressETB.visible(true);
+    currentAddressNTB.visible(false);
+  } else {
+    currentAddressETB.visible(false);
+    currentAddressNTB.visible(true);
+  }
 
   // otHelpText-appending the masked registerMobileNumber -
   const otpHelpText = document.getElementsByClassName('form-otphelptext')[0];
@@ -98,7 +125,8 @@ const OTPGEN = {
     return (res?.otpGenResponse?.status?.errorCode === '0') ? otpGenSuccess(res, globals) : otpGenFailure(res, globals);
   },
   errorCallback(err, globals) {
-    console.log(`I am in errorCallback ${globals}`);
+    otpGenFailure(err, globals);
+    console.log(`I am in errorCallback OTP GEN ${globals}`);
   },
   path: urlPath('/content/hdfc_ccforms/api/customeridentificationV4.json'),
   loadingText: 'Please wait while we are authenticating you',
@@ -131,6 +159,53 @@ const otpValSuccess = (res, globals) => {
   loginPanel.visible(false);
   otpPanel.visible(false);
   ccWizardPannel.visible(true);
+  if (currentFormContext.existingCustomer === 'Y') {
+    // prefilling logic to start
+    // Gender prefill
+    const custGender = res.otpValidationResponse.demogResponse.BRECheckAndFetchDemogResponse.VDCUSTGENDER;
+    // eslint-disable-next-line no-nested-ternary
+    const Gender = (custGender === 'M') ? 'Male' : (custGender === 'F') ? 'Female' : 'Others';
+    globals.functions.setProperty(globals.form.corporateCardWizardView.yourDetailsPanel.yourDetailsPage.personalDetails.gender, { value: Gender });
+
+    // First name ,last name,Middle name prefilling
+    const FullName = res.otpValidationResponse.demogResponse.BRECheckAndFetchDemogResponse.VDCUSTFULLNAME;
+    const firstName = FullName.split(' ')[0];
+    let middleName; let lastName;
+    if (FullName.split(' ').length - 1 >= 2) {
+      middleName = FullName.split(' ')[1];
+      lastName = FullName.value.split(' ')[FullName.split(' ').length - 1];
+    } else {
+      lastName = FullName.split(' ')[1];
+      middleName = '';
+    }
+
+    globals.functions.setProperty(globals.form.corporateCardWizardView.yourDetailsPanel.yourDetailsPage.personalDetails.firstName, { value: firstName });
+    globals.functions.setProperty(globals.form.corporateCardWizardView.yourDetailsPanel.yourDetailsPage.personalDetails.lastName, { value: lastName });
+    globals.functions.setProperty(globals.form.corporateCardWizardView.yourDetailsPanel.yourDetailsPage.personalDetails.middleName, { value: middleName });
+
+    // Date/Pan prefilling
+    const convertDateFormat = (date) => {
+      const year = date.slice(0, 4);
+      const month = date.slice(4, 6).padStart(2, '0');
+      const day = date.slice(6, 8).padStart(2, '0');
+      const options = { month: 'short', day: 'numeric', year: 'numeric' };
+      return new Date(year, month - 1, day).toLocaleDateString('en-US', options);
+    };
+    if (pannel.login.pan.$value) {
+      const custDate = res.otpValidationResponse.demogResponse.BRECheckAndFetchDemogResponse.DDCUSTDATEOFBIRTH;
+      globals.functions.setProperty(globals.form.corporateCardWizardView.yourDetailsPanel.yourDetailsPage.personalDetails.dobPersonalDetails, { value: convertDateFormat(custDate.toString()) });
+    } else {
+      // pan prefilling
+      const panValue = res.otpValidationResponse.demogResponse.BRECheckAndFetchDemogResponse.VDCUSTITNBR;
+      globals.functions.setProperty(globals.form.corporateCardWizardView.yourDetailsPanel.yourDetailsPage.personalDetails.dobPersonalDetails, { value: panValue });
+    }
+
+    // Address line prefilling
+    const AddressPrefill = `${res.otpValidationResponse.demogResponse.BRECheckAndFetchDemogResponse.VDCUSTADD1},${res.otpValidationResponse.demogResponse.BRECheckAndFetchDemogResponse.VDCUSTADD2},${res.otpValidationResponse.demogResponse.BRECheckAndFetchDemogResponse.VDCUSTADD3},${res.otpValidationResponse.demogResponse.BRECheckAndFetchDemogResponse.VDCUSTCITY},${
+      res.otpValidationResponse.demogResponse.BRECheckAndFetchDemogResponse.VDCUSTSTATE},${
+      res.otpValidationResponse.demogResponse.BRECheckAndFetchDemogResponse.VDCUSTZIPCODE}`;
+    globals.functions.setProperty(globals.form.corporateCardWizardView.yourDetailsPanel.currentDetails.currentAddressETB.prefilledCurrentAdddress, { value: AddressPrefill });
+  }
   (async () => {
     const myImportedModule = await import('./cc.js');
     myImportedModule.onWizardInit();
@@ -193,6 +268,7 @@ const OTPVAL = {
     return (res?.otpGenResponse?.status?.errorCode === '0') ? otpValSuccess(res, globals) : otpValFailure(res, globals);
   },
   errorCallback(err, globals) {
+    otpValFailure(err, globals);
     console.log(`I am in errorCallback_OtpFailure ${globals}`);
   },
   path: urlPath('/content/hdfc_cc_unified/api/otpValFetchAssetDemog.json'),
