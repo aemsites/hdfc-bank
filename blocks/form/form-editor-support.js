07a0fc6144f8b825fea02351c064bab24f2b7164
Nitin Gupta <nitigupt@adobe.com>
2024-03-16 18:03:11 +0530

    annotate complete form

--- blocks/form/form-editor-support.js
+++ blocks/form/form-editor-support.js
@@ -6,28 +6,32 @@ export function annotateFormForEditing(formEl, formDefinition) {
     let formFieldMap = {};
     function annotateForm(mutationsList) {
         for (const mutation of mutationsList) {
-            if (mutation.type === "childList" || mutation.type === "subtree") {
-                mutation.addedNodes.forEach((fieldWrapper) => {
-                    if (fieldWrapper.classList.contains("field-wrapper")) {
-                        console.log(fieldWrapper);
-                        const id = fieldWrapper.id;
-                        const fd = getFieldById(formDefinition[":items"], id);
-                        if (fd && fd.properties) {
-                            fieldWrapper.setAttribute('data-aue-type', 'component');
-                            fieldWrapper.setAttribute('data-aue-resource', `urn:aemconnection:${fd.properties["fd:path"]}`);
-                            fieldWrapper.setAttribute('data-aue-model', fd.fieldType === 'image'? 'form-image': fd.fieldType);
-                            fieldWrapper.setAttribute('data-aue-label', fd.name);
-                        } else {
-                            console.warn(`field ${id} not found in form definition`);
-                        }
-                        if (fieldWrapper.classList.contains("form-panel-wrapper")) {
-                            fieldWrapper.setAttribute('data-aue-type', 'container');
-                            fieldWrapper.setAttribute('data-aue-behavior', 'component');
-                        }
-                    }
-                });
+            if (mutation.type === "childList") {
+                annotateItems(mutation.addedNodes);
             }
         }
+    }
+    function annotateItems(items) {
+        items.forEach((fieldWrapper) => {
+            if (fieldWrapper.classList.contains("field-wrapper")) {
+                console.log(fieldWrapper);
+                const id = fieldWrapper.id;
+                const fd = getFieldById(formDefinition[":items"], id);
+                if (fd && fd.properties) {
+                    fieldWrapper.setAttribute('data-aue-type', 'component');
+                    fieldWrapper.setAttribute('data-aue-resource', `urn:aemconnection:${fd.properties["fd:path"]}`);
+                    fieldWrapper.setAttribute('data-aue-model', fd.fieldType === 'image'? 'form-image': fd.fieldType);
+                    fieldWrapper.setAttribute('data-aue-label', fd.name);
+                } else {
+                    console.warn(`field ${id} not found in form definition`);
+                }
+                if (fieldWrapper.classList.contains("form-panel-wrapper")) {
+                    fieldWrapper.setAttribute('data-aue-type', 'container');
+                    fieldWrapper.setAttribute('data-aue-behavior', 'component');
+                    annotateItems(fieldWrapper.childNodes);
+                }
+            }
+        });
     }
 
     function getFieldById(items, id) {
