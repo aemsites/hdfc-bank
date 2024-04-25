4c0610e9b25f20d158f1734b5e802fbe3db8116e
Suparna Bose <suparnab@adobe.com>
2024-04-25 19:14:45 +0530

    resolved with comments

--- blocks/form/mappings.js
+++ blocks/form/mappings.js
@@ -12,16 +12,12 @@ export default async function componentDecorator(fd) {
     const module = await import('./components/wizard.js');
     return module.default;
   }
-  if (type.includes('accordion')) {
+  if (fd.appliedCssClassNames?.includes('hdfc-accordian')) {
     const module = await import('./components/accordion.js');
     return module.default;
   }
   if (fd.appliedCssClassNames === 'passwordField') {
     const module = await import('./components/passwordField.js');
-    return module.default;
-  }
-  if (fd.appliedCssClassNames?.includes('futureDate-disabled')) {
-    const module = await import('./components/dateField.js');
     return module.default;
   }
   if ((fieldType?.includes('input') || fieldType === 'drop-down') && fd.appliedCssClassNames !== 'passwordField') {
