8afb0339988d76f36005a5af71aae210f097265a
srivastava21ashish <srivastava21ashish@gmail.com>
2024-05-03 17:02:30 +0530

    card image

--- common/executeinterfaceutils.js
+++ common/executeinterfaceutils.js
@@ -26,7 +26,7 @@ const formatDate = (inputDate) => {
   const date = new Date(inputDate);
 
   const day = date.getDate();
-  const month = date.toLocaleString('default', { month: 'short' }).substring(0, 3);;
+  const month = date.toLocaleString('default', { month: 'short' }).substring(0, 3); 
   const year = date.getFullYear();
 
   const formattedDate = `${day}-${month}-${year}`;
