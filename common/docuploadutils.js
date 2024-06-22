c4663c3735533690e3e1d622d201f3d37bd15996
bazilMuralidharanDev <bazil.m@dizitalpods.com>
2024-06-22 19:11:26 +0530

    doc-upload-api baseCode with mild clean up

--- common/docuploadutils.js
+++ common/docuploadutils.js
@@ -3,21 +3,9 @@ import corpCreditCard from './constants.js';
 import { corpCreditCardContext } from './journey-utils.js';
 import {
   displayLoader,
-  getJsonResponse,
   hideLoaderGif,
-  restAPICall,
 } from './makeRestAPI.js';
-
-/**
- * Convert file into binary
- * @param {object} data - fileObject
- * @returns {BinaryData} - binary formated data
- */
-const binaryFormat = (data) => {
-  const file = new FileReader();
-  file.readAsText(data);
-  return file.result;
-};
+import { urlPath, generateUUID } from './formutils.js';
 
 /**
  * Uploads multiple files asynchronously and returns an array of responses.
@@ -26,55 +14,72 @@ const binaryFormat = (data) => {
  * @param {string} method - The HTTP method ('GET', 'POST', etc.) for the API request.
  * @returns {Promise<Array>} - Promise that resolves to an array of responses from each file upload.
  */
-const uploadMultipleFileCall = (uploadFiles, apiUrl, method) => {
+const uploadMultipleFileCall = async (uploadFiles, apiUrl, method) => {
   displayLoader();
-  const fileResponse = [];
-  uploadFiles?.forEach(async (payloadData, i) => {
+  const promises = uploadFiles?.map(async (formData) => {
     try {
-      const response = await getJsonResponse(apiUrl, payloadData, method);
-      fileResponse[i] = response;
+      const response = await fetch(apiUrl, {
+        method,
+        body: formData,
+      });
+      const data = await response.json();
+      return data;
     } catch (error) {
-      hideLoaderGif();
-      fileResponse[i] = error;
+      return error;
     }
   });
-  return fileResponse;
+  const fileResponses = await Promise.allSettled(promises);
+  hideLoaderGif();
+  return fileResponses;
 };
 
-const createDocPayload = ({
-  docValue,
-  docType,
-  fileId,
-  globals,
-}) => {
-  const file = docValue?.$value?.data;
-  const fileBinaryFormat = binaryFormat(file);
-  debugger;
-  const { currentFormContext } = corpCreditCardContext;
-  const { breDemogResponse } = currentFormContext; // requestNumber
-  const executeInterfaceRes = currentFormContext.executeInterfaceResPayload; // applicationRefNo
-  const { journeyID } = currentFormContext;
-  const { requestNumber } = breDemogResponse;
-  const { applicationRefNo } = executeInterfaceRes;
-  const { journeyName } = corpCreditCard;
-  const mobileNumber = globals.form.loginPanel.mobilePanel.registeredMobileNumber.$value;
-  const { userAgent } = window.navigator;
-  const documentName = file.data.name;
-  const formData = new FormData();
-  formData.append('548be82c-a968-467c-8e35-7db7610dffca', fileBinaryFormat); // idKey-?
-  formData.append('imageBinary', fileBinaryFormat);
-  formData.append('documentType', docType);
-  formData.append('journeyID', journeyID);
-  formData.append('requestNumber', requestNumber);
-  formData.append('applicationRefNo', applicationRefNo);
-  formData.append('journeyName', journeyName);
-  formData.append('mobileNumber', mobileNumber);
-  formData.append('userAgent', userAgent);
-  formData.append('documentName', documentName);
-  formData.append('548be82c-a968-467c-8e35-7db7610dffca', fileBinaryFormat); // idKey-?
-  formData.append(fileId, fileBinaryFormat); // idKey-? is it '1_FS' or '2_BS'
-  formData.append(`existing_${fileId}`, ''); // idKey-? is it '1_FS' or '2_BS'
-  return formData;
+/**
+ * Creates a FormData payload for document upload.
+ *
+ * @param {Object} param0 - Parameters object.
+ * @param {Object} param0.docValue - Document value object containing data to upload.
+ * @param {string} param0.docType - Type of the document.
+ * @param {string} param0.fileId - Unique identifier for the file.
+ * @returns {Promise<FormData>} FormData object containing the document payload.
+ * @throws {Error} Throws an error if document creation fails.
+ */
+const createDocPayload = async ({ docValue, docType, fileId }) => {
+  try {
+    const {
+      currentFormContext: {
+        journeyName,
+        journeyID,
+        eRefNumber,
+        breDemogResponse: { MOBILE },
+        executeInterfaceResPayload: { applicationRefNumber },
+      },
+    } = corpCreditCardContext;
+    const file = docValue?.$value?.data;
+    const fileBinary = file;
+    const documentName = file.name;
+    const mobileNumber = String(MOBILE);
+    const { userAgent } = window.navigator;
+    const uuId = generateUUID();
+    const payloadKeyValuePairs = {
+      [`${uuId}`]: fileBinary,
+      imageBinary: fileBinary,
+      docuemntType: docType,
+      journeyID,
+      requestNumber: eRefNumber,
+      applicationRefNo: applicationRefNumber,
+      journeyName,
+      mobileNumber,
+      userAgent,
+      docuemntName: documentName,
+      fileId: uuId,
+      [`existing_${fileId}`]: '',
+    };
+    const formData = new FormData();
+    Object.entries(payloadKeyValuePairs).forEach(([key, value]) => formData.append(key?.toString(), value));
+    return formData;
+  } catch (error) {
+    throw new Error('Failed to create document payload');
+  }
 };
 
 /**
@@ -82,48 +87,38 @@ const createDocPayload = ({
  * @param {object} globals - The global object containing necessary globals form data
  */
 const documentUpload = async (globals) => {
-  debugger;
-  // form.corporateCardWizardView.selectKycPanel.docUploadETBFlow.DocUploadBack
-  // const frontFile = globals.form.corporateCardWizardView.selectKycPanel.docUploadETBFlow.DocUploadFront.$value;
-  // const backFile = globals.form.corporateCardWizardView.selectKycPanel.docUploadETBFlow.DocUploadBack.$value;
-  // const documentType = globals.form.corporateCardWizardView.selectKycPanel.docUploadETBFlow.docUploadDropdown.$value;
   const fsId = '1_FS';
   const bsId = '1_BS';
-  const { selectKycPanel: { docUploadETBFlow: { DocUploadFront, DocUploadBack, docUploadDropdown } } } = globals.form.corporateCardWizardView;
+  const {
+    selectKycPanel: {
+      docUploadETBFlow: { DocUploadFront, DocUploadBack, docUploadDropdown },
+    },
+  } = globals.form.corporateCardWizardView;
   const docType = docUploadDropdown.$value;
   const frontDoc = {
-    DocUploadFront,
+    docValue: DocUploadFront,
     docType,
-    fsId,
-    globals,
+    fileId: fsId,
   };
   const backDoc = {
-    DocUploadBack,
+    docValue: DocUploadBack,
     docType,
-    bsId,
-    globals,
+    fileId: bsId,
   };
-  const fsFilePayload = createDocPayload(frontDoc);
-  const bsFilePayload = createDocPayload(backDoc);
-  const apiEndPoint = corpCreditCard.endpoints.docUpload;
+  const apiEndPoint = urlPath(corpCreditCard.endpoints.docUpload);
   const method = 'POST';
-  const eventHandlers = {
-    successCallBack(res, globalObj) {
-      debugger;
-      console.log(res, globalObj);
-    },
-    errorCallBack(res, globalObj) {
-      debugger;
-      console.log(res, globalObj);
-    },
-  };
-  restAPICall(globals, 'POST', fsFilePayload, apiEndPoint, eventHandlers.successCallBack, eventHandlers.errorCallBack);
-
-  // need to use 👇 further for multiple file api handling and after effect methods.
   try {
-    const [fsFileRes, bsFileRes] = await uploadMultipleFileCall([fsFilePayload, bsFilePayload], apiEndPoint, method);
+    const fsFilePayload = await createDocPayload(frontDoc);
+    const bsFilePayload = await createDocPayload(backDoc);
+    const [fsFileResponse, bsFileResponse] = await uploadMultipleFileCall(
+      [fsFilePayload, bsFilePayload],
+      apiEndPoint,
+      method,
+    );
+    console.log(fsFileResponse, 'fs-file-1-response');
+    console.log(bsFileResponse, 'bs-file-1-response');
   } catch (error) {
-    console.log(error);
+    console.log('errorInFilePayload');
   }
 };
 
