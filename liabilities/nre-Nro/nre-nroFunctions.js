5adf017622ccdf1e3061264ad87a32404df4e409
reddymanojkumar653 <reddymanojkumar653@gmail.com>
2024-11-25 16:40:42 +0530

    analytics observation fixes

--- liabilities/nre-Nro/nre-nroFunctions.js
+++ liabilities/nre-Nro/nre-nroFunctions.js
@@ -191,6 +191,9 @@ const getCountryName = (countryCodeIst) => new Promise((resolve) => {
 });
 
 function errorHandling(response, journeyState, globals) {
+  setTimeout(() => {
+    Promise.resolve(sendAnalytics('page load-Error Page', { }, 'ON_ERROR_PAGE_LOAD', globals));
+  }, 2000);
   const {
     mobileNumber,
     leadProfileId,
@@ -2181,7 +2184,7 @@ function nreNroAccountType(nroAccountTypePanel, nreAccountTypePanel, globals) {
 function multiAccountVarient(selectAccount, globals) {
   const varientType = currentFormContext.journeyAccountType;
   globals.functions.setProperty(selectAccount.multipleAccounts, { visible: false });
-  // globals.functions.setProperty(globals.form.wizardPanel.continue, { visible: true });
+  globals.functions.setProperty(globals.form.wizardPanel.continue, { visible: true });
   globals.functions.setProperty(globals.form.wizardPanel.MultiAccoCountinue, { visible: false });
   globals.functions.setProperty(selectAccount.text, { visible: false });
   globals.functions.setProperty(selectAccount.customerName, { visible: false });
