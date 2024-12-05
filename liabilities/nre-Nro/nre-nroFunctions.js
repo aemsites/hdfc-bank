35d089afa1849eb14e070bf8ba0fd2901af54de3
srivastava21ashish <srivastava21ashish@gmail.com>
2024-12-05 14:27:30 +0530

    change

--- liabilities/nre-Nro/nre-nroFunctions.js
+++ liabilities/nre-Nro/nre-nroFunctions.js
@@ -377,7 +377,7 @@ const getOtpNRE = async (mobileNumber, pan, dob, globals) => {
   const jidTemporary = createJourneyId(VISIT_MODE, JOURNEY_NAME, CHANNEL, globals);
   /* jidTemporary  temporarily added for FD development it has to be removed completely once runtime create journey id is done with FD */
   //const jidTemporary = createJourneyId(VISIT_MODE, JOURNEY_NAME, CHANNEL, globals);
-  const sessionInitPromise = sessionService(jidTemporary);
+  const sessionInitPromise = await sessionService(jidTemporary);
   Promise.resolve(sessionInitPromise)
   .then((res) => {
     console.log(res);
