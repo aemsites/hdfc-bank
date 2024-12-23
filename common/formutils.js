483c26a3614e317a80406e1637500e0ee635e882
kunal-adobe <kunalj.adobe@gmail.com>
2024-12-23 15:07:27 +0530

    update eref num generation

--- common/formutils.js
+++ common/formutils.js
@@ -698,16 +698,10 @@ function createDeepCopyFromBlueprint(blueprint) {
 }
 
 const generateErefNumber = () => {
-  const now = new Date();
-
-  const year = String(now.getFullYear());
-  const month = String(now.getMonth() + 1).padStart(2, '0');
-  const day = String(now.getDate()).padStart(2, '0');
-  const hours = String(now.getHours()).padStart(2, '0');
-  const minutes = String(now.getMinutes()).padStart(2, '0');
-  const firstDigitOfSeconds = String(now.getSeconds()).charAt(0);
-
-  return `AD${year}${month}${day}${hours}${minutes}${firstDigitOfSeconds}`;
+  const timestamp = Date.now().toString(); // Current time in milliseconds
+  const randomComponent = Math.floor(Math.random() * 1000).toString().padStart(3, '0'); // Random 3-digit number
+  const uniqueNumber = timestamp.slice(-9) + randomComponent; // Take the last 9 digits of timestamp + 3 random digits
+  return `AD${uniqueNumber}`;
 };
 
 /**
