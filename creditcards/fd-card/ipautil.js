b5211db4f94ee995d774a15dd8d42bbc17626bf2
kunal-adobe <kunalj.adobe@gmail.com>
2025-01-06 16:56:41 +0530

    removed recursive for testing

--- creditcards/fd-card/ipautil.js
+++ creditcards/fd-card/ipautil.js
@@ -2,6 +2,7 @@ import { BASEURL, CURRENT_FORM_CONTEXT, FORM_RUNTIME } from '../../common/consta
 import { applicableCards, extractJSONFromHTMLString, urlPath } from '../../common/formutils.js';
 import { invokeJourneyDropOffUpdate } from './fd-journey-util.js';
 import {
+  fetchJsonResponse,
   fetchRecursiveResponse,
 } from '../../common/makeRestAPI.js';
 import { FD_ENDPOINTS } from './constant.js';
@@ -50,8 +51,9 @@ const ipa = (payload, showLoader, hideLoader, globals) => {
   const ipaRequest = createIpaRequest(payload, globals);
   const apiEndPoint = urlPath(FD_ENDPOINTS.ipa);
   if (showLoader) FORM_RUNTIME.ipa();
-  const fieldName = ['IPAResponse', 'productEligibility', 'productDetails'];
-  return fetchRecursiveResponse('ipa', apiEndPoint, ipaRequest, 'POST', Number(payload.ipaDuration), Number(payload.ipaTimer), fieldName, hideLoader);
+  // const fieldName = ['IPAResponse', 'productEligibility', 'productDetails'];
+  // return fetchRecursiveResponse('ipa', apiEndPoint, ipaRequest, 'POST', Number(payload.ipaDuration), Number(payload.ipaTimer), fieldName, hideLoader);
+  return fetchJsonResponse(apiEndPoint, ipaRequest, 'POST', hideLoader);
 };
 
 const updateData = (globals, productDetail, panel, index) => {
