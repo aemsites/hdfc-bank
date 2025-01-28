bb6130bc9ed199c85297f9373c376ad2422968b1
bazilMuralidharanDev <bazil.m@dizitalpods.com>
2025-01-28 15:25:34 +0530

    modified form-constant and semi-styles header css coded and commented

--- scripts/form-constant.js
+++ scripts/form-constant.js
@@ -1,4 +1,5 @@
 import semiFormContant from '../creditcards/semi/semi-form-constant.js';
+// import ccFormConstant from '../creditcards/corporate-creditcard/cc-form-constant.js';
 
 /**
  * Similar to semiFormConstant, all form constants should be stored in their specific form folders.
@@ -16,6 +17,7 @@ const ccFormConstant = {
     prod: 'https://assets.adobedtm.com/80673311e435/029b16140ccd/launch-39d52f236cd6.min.js',
     loadTime: 1200,
   },
+  stylePath: '',
   functionsExportPath: '../../../creditcards/corporate-creditcard/cc-functions.js',
   functionsFormPaths: [
     '/content/forms/af/hdfc_haf/cards/corporatecreditcard/uat/hdfc',
@@ -24,37 +26,42 @@ const ccFormConstant = {
   ],
 };
 
-const otherForms = {
-  '/content/forms/af/hdfc_haf/digital/etb-fixed-deposit-cc': '../../../creditcards/fd-card/fd-functions.js', // fd
-  '/content/forms/af/hdfc_haf/digital/pvtestfdliencugtest': '../../../creditcards/fd-card/fd-functions.js', // fd
-  '/content/forms/af/hdfc_haf/digital/fd-lien-cug': '../../../creditcards/fd-card/fd-functions.js', // fd
-  '/content/forms/af/hdfc_haf/digital/fdlienprodtest': '../../../creditcards/fd-card/fd-functions.js', // fd form
-  '/content/forms/af/hdfc_haf/digital/account-opening-nre-nro': '../../../liabilities/nre-Nro/nre-nro.js',
+const nreFormConstant = {
+  // NRE NRO
+  formPath: ['nre-nro', 'account-opening-nre-nro'],
+  class: 'nre',
+  urlKey: ['nre-nro', 'account-opening-nre-nro'],
+  launchScript: {
+    dev: 'https://assets.adobedtm.com/80673311e435/029b16140ccd/launch-e17de29eec01-development.min.js',
+    prod: 'https://assets.adobedtm.com/80673311e435/029b16140ccd/launch-39d52f236cd6.min.js',
+    loadTime: 3600,
+  },
+  stylePath: '',
+  functionsExportPath: '../../../liabilities/nre-Nro/nre-nro.js',
+  functionsFormPaths: [
+    '/content/forms/af/hdfc_haf/digital/account-opening-nre-nro',
+  ],
 };
-const otherFormConst = [
-  ccFormConstant,
-  {
-    // NRE NRO
-    formPath: ['nre-nro', 'account-opening-nre-nro'],
-    class: 'nre',
-    urlKey: ['nre-nro', 'account-opening-nre-nro'],
-    launchScript: {
-      dev: 'https://assets.adobedtm.com/80673311e435/029b16140ccd/launch-e17de29eec01-development.min.js',
-      prod: 'https://assets.adobedtm.com/80673311e435/029b16140ccd/launch-39d52f236cd6.min.js',
-      loadTime: 3600,
-    },
+
+const fdFormConstant = {
+  // FD
+  formPath: ['etb-fixed-deposit-cc', 'pvtestfdliencugtest', 'fd-lien-cug-test', 'fdlienprodtest'],
+  class: 'fdlien',
+  urlKey: ['fdlien', 'pvtestfdliencugtest', 'fd-lien-cug-test', 'etb-fixed-deposit-cc', 'fdlienprodtest'],
+  launchScript: {
+    dev: 'https://assets.adobedtm.com/80673311e435/029b16140ccd/launch-a47f215bcdb9-development.min.js',
+    prod: 'https://assets.adobedtm.com/80673311e435/029b16140ccd/launch-39d52f236cd6.min.js',
+    loadTime: 1200,
   },
-  {
-    formPath: ['etb-fixed-deposit-cc', 'pvtestfdliencugtest', 'fd-lien-cug-test', 'fdlienprodtest'],
-    class: 'fdlien',
-    urlKey: ['fdlien', 'pvtestfdliencugtest', 'fd-lien-cug-test', 'etb-fixed-deposit-cc', 'fdlienprodtest'],
-    launchScript: {
-      dev: 'https://assets.adobedtm.com/80673311e435/029b16140ccd/launch-a47f215bcdb9-development.min.js',
-      prod: 'https://assets.adobedtm.com/80673311e435/029b16140ccd/launch-39d52f236cd6.min.js',
-      loadTime: 1200,
-    },
-  },
-];
+  stylePath: '',
+  functionsExportPath: '../../../creditcards/fd-card/fd-functions.js', // fd
+  functionsFormPaths: [
+    '/content/forms/af/hdfc_haf/digital/etb-fixed-deposit-cc',
+    '/content/forms/af/hdfc_haf/digital/pvtestfdliencugtest',
+    '/content/forms/af/hdfc_haf/digital/fd-lien-cug',
+    '/content/forms/af/hdfc_haf/digital/fdlienprodtest',
+  ],
+};
 
 const mapFormPath = (paths, rootPath) => paths?.reduce((acc, path) => {
   acc[path] = rootPath;
@@ -63,16 +70,21 @@ const mapFormPath = (paths, rootPath) => paths?.reduce((acc, path) => {
 
 const semiFormPath = mapFormPath(semiFormContant?.functionsFormPaths, semiFormContant?.functionsExportPath);
 const ccFormPath = mapFormPath(ccFormConstant?.functionsFormPaths, ccFormConstant?.functionsExportPath);
+const nreFormPath = mapFormPath(nreFormConstant?.functionsFormPaths, nreFormConstant?.functionsExportPath);
+const fdFormPath = mapFormPath(fdFormConstant?.functionsFormPaths, fdFormConstant?.functionsExportPath);
 
 const formIdPathMapping = {
-  ...otherForms,
   ...semiFormPath,
   ...ccFormPath,
+  ...fdFormPath,
+  ...nreFormPath,
 };
 
 const FORM_CONSTANT = [
   semiFormContant,
-  ...otherFormConst,
+  ccFormConstant,
+  fdFormConstant,
+  nreFormConstant,
 ];
 
 export {
