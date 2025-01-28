9b2eeb95830f1dd4188469a560deacdec32b5daf
Suresh R <adobesure24@gmail.com>
2025-01-28 13:39:27 +0530

    Error Handling Account Opening API

--- liabilities/fd-external-funding/fd-ef-error-handler.js
+++ liabilities/fd-external-funding/fd-ef-error-handler.js
@@ -1,10 +1,15 @@
 
 import * as FD_EF_CONSTANT from './constant.js';
 import { displayLoader, hideLoaderGif } from '../../common/makeRestAPI.js';
+import {
+  invokeJourneyDropOff,
+  invokeJourneyDropOffUpdate,
+  effdInvokeJourneyDropOffByParam,
+} from './fd-external-funding-journey-utils.js';
 const {
   CURRENT_FORM_CONTEXT: currentFormContext,
 } = FD_EF_CONSTANT;
-function errorHandlingFDExt(response, journeyState, globals) {
+function errorHandlingFDExt(response, globals) {
     hideLoaderGif();
     const {
       mobileNumber,
@@ -12,7 +17,14 @@ function errorHandlingFDExt(response, journeyState, globals) {
     } = currentFormContext;
     if (response.errorCode === '02') {
       globals.functions.setProperty(globals.form.otppanelwrapper.otpFragment.incorrectOTPText, { visible: true });
-    }  
+    } else if (response?.status?.errorCode === 'AD_SSV999') {
+      const {bannerImagePanel} = globals.form;
+      globals.functions.setProperty(bannerImagePanel, { visible: false });
+      globals.functions.setProperty(globals.form.thankYouPanel, { visible: false });
+      globals.functions.setProperty(globals.form.resultPanel, { visible: true });
+      globals.functions.setProperty(globals.form.resultPanel.commonErrorPanel, { visible: true });
+    }
+    
     invokeJourneyDropOffUpdate(journeyState, mobileNumber, journeyID, globals);
   }
 
